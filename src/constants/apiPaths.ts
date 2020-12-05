06fa48431ec6c180aa0d96e8d15eee88edf5e738
Dzmitry Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2020-12-06 00:58:00 +0300

    refactor: Task 8 updates

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,13 +1,14 @@
 
 const API_PATHS = {
-  back_url: 'https://27uu1xwip2.execute-api.eu-west-1.amazonaws.com/dev',
+  back_url: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
   admin: {
-    product: "https://27uu1xwip2.execute-api.eu-west-1.amazonaws.com/dev/admin/product"
+    product: "https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev/admin/product"
   },
-  product: 'https://27uu1xwip2.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://27uu1xwip2.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://6bli2nbdj9.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://27uu1xwip2.execute-api.eu-west-1.amazonaws.com/dev'
+  product: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ll4y2b5ev0.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://karneyenkadzmitry-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
