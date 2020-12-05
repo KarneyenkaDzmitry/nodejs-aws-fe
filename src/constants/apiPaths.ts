186cf95363a89e89f2afeab5e3dcbc5220012927
Dzmitry Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2020-12-06 01:04:57 +0300

    refactor: update with cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -5,7 +5,7 @@ const API_PATHS = {
     product: "https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev/admin/product"
   },
   product: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'http://karneyenkadzmitry-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
   import: 'https://ll4y2b5ev0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://9pj3zddvmb.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'http://karneyenkadzmitry-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
